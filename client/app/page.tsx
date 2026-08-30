d4dc8e9d8f4d017f9bb5893290d04fc027ee3c0a
Priyanshu Patel <patelanshu702@gmail.com>
2026-08-30 14:29:49 +0530

    personal_v1

---
+++ client/app/page.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from "@/components/ui/mode-toogle";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <ModeToggle>
+    
+   </ModeToggle>
+  );
+}
