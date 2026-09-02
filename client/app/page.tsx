627f227b5cda6cdd441035520f2c5e3d51ff568b
Priyanshu Patel <patelanshu702@gmail.com>
2026-09-02 11:54:25 +0530

    office_v1

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,4 +1,4 @@
-import { ModeToggle } from "@/components/ui/mode-toogle";
+import { ModeToggle } from "@/components/ui/mode-toggle";
 import Image from "next/image";
 
 export default function Home() {
